84f0e9e4b6aca96f3db57ef7c9e9849d46cc9800
TOFAZZAL27 <107051624+TOFAZZAL27@users.noreply.github.com>
2023-06-30 12:11:32 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+//--------side menu js start----------//
+ 
+ function openNav() {
+ document.getElementById("mySidenav").style.width = "250px";
+ }
+ 
+ function closeNav() {
+ document.getElementById("mySidenav").style.width = "0";
+ }
+
+ //-------side minu js end------------//
